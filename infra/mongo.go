6d3f37b67c8711f2a0045f0656ecb01cfc8ec3d7
Alexandre Paes <alexandre_slp@poli.ufrj.br>
2022-01-13 11:54:05 -0300

    added mongoservice initial structure

--- infra/mongo.go
+++ infra/mongo.go
@@ -1,1 +1,32 @@
 package infra
+
+func NewMongoService(connString string) *mongoService {
+	return &mongoService{
+		connectionString: connString,
+	}
+}
+
+//mongoService Service responsible for interacting with mongoDB
+type mongoService struct {
+	connectionString string
+}
+
+func (ms mongoService) addEvent() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (ms mongoService) updateEvent() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (ms mongoService) removeEvent() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (ms mongoService) healthCheck() {
+	//TODO implement me
+	panic("implement me")
+}
